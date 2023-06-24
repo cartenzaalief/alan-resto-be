17e609646f60dd3a1904c8012869b0eb82b6acc9
cartenzaalief <cartenzaalief@gmail.com>
2023-06-25 01:42:49 +0700

    config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
