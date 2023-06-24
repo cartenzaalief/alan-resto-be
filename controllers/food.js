e3455d3e1262cd9fa4d5a03c7a430ad7f2b73ca4
cartenzaalief <cartenzaalief@gmail.com>
2023-06-25 03:12:04 +0700

    config

--- controllers/food.js
+++ controllers/food.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = {
   add: (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./tmp/db.json"));
+    let data = JSON.parse(fs.readFileSync("./db.json"));
     let newData = {};
     if (data.foods.length < 1) {
       newData.id = 1;
@@ -15,20 +15,19 @@ module.exports = {
       newData.name = req.body.name;
       newData.price = req.body.price;
     }
-    console.log(newData);
     data.foods.push(newData);
-    fs.writeFileSync("./tmp/db.json", JSON.stringify(data));
+    fs.writeFileSync("./db.json", JSON.stringify(data));
     return res.status(200).send({
       success: true,
       message: "Success.",
     });
   },
   data: (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./tmp/db.json"));
+    let data = JSON.parse(fs.readFileSync("./db.json"));
     return res.status(200).send(data.foods);
   },
   delete: (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./tmp/db.json"));
+    let data = JSON.parse(fs.readFileSync("./db.json"));
     let getIndex = data.foods.findIndex((val) => val.id == req.params.id);
     if (getIndex < 0) {
       res.status(200).send({
@@ -37,7 +36,7 @@ module.exports = {
       });
     } else {
       data.foods.splice(getIndex, 1);
-      fs.writeFileSync("./tmp/db.json", JSON.stringify(data));
+      fs.writeFileSync("./db.json", JSON.stringify(data));
       res.status(200).send({
         success: true,
         message: "Success."
