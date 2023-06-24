570bedf893182427d9864a91ebe37ea1c8271b2b
cartenzaalief <cartenzaalief@gmail.com>
2023-06-25 03:05:38 +0700

    config

--- controllers/food.js
+++ controllers/food.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = {
   add: (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./db.json"));
+    let data = JSON.parse(fs.readFileSync("./tmp/db.json"));
     let newData = {};
     if (data.foods.length < 1) {
       newData.id = 1;
@@ -17,18 +17,18 @@ module.exports = {
     }
     console.log(newData);
     data.foods.push(newData);
-    fs.writeFileSync("./db.json", JSON.stringify(data));
+    fs.writeFileSync("./tmp/db.json", JSON.stringify(data));
     return res.status(200).send({
       success: true,
       message: "Success.",
     });
   },
   data: (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./db.json"));
+    let data = JSON.parse(fs.readFileSync("./tmp/db.json"));
     return res.status(200).send(data.foods);
   },
   delete: (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./db.json"));
+    let data = JSON.parse(fs.readFileSync("./tmp/db.json"));
     let getIndex = data.foods.findIndex((val) => val.id == req.params.id);
     if (getIndex < 0) {
       res.status(200).send({
@@ -37,7 +37,7 @@ module.exports = {
       });
     } else {
       data.foods.splice(getIndex, 1);
-      fs.writeFileSync("./db.json", JSON.stringify(data));
+      fs.writeFileSync("./tmp/db.json", JSON.stringify(data));
       res.status(200).send({
         success: true,
         message: "Success."
--- index.js
+++ index.js
@@ -6,18 +6,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://alan-resto.netlify.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(cors());
 app.use("/api", express.static("public"));
 
 app.get("/api", (req, res) => {
