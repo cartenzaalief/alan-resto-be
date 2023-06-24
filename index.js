86b934f6aff7824060d97011851e9a7e7670a795
cartenzaalief <cartenzaalief@gmail.com>
2023-06-25 02:32:16 +0700

    config

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "https://alan-resto.netlify.app", credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use("/api", express.static("public"));
 
 app.get("/api", (req, res) => {
